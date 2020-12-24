3fade00fe7c4a39430121fe1ded522ec3e080a27
Simon Fletcher <simonfletcher0@gmail.com>
2020-12-24 15:46:56 +0000

    make solution more idomatic

--- years/2020/01/index.ts
+++ years/2020/01/index.ts
@@ -2,18 +2,7 @@ import { calculate } from './solutions'
 import { input } from '../../../utils/helpers'
 
 const init = () => {
-  console.log(
-    calculate(
-      input(__dirname + `/input.txt`).map(v => parseInt(v)),
-      2,
-    ),
-  )
-
-  console.log(
-    calculate(
-      input(__dirname + `/input.txt`).map(v => parseInt(v)),
-      3,
-    ),
-  )
+  console.log(calculate(input(`${__dirname}/input.txt`).map(Number), 2))
+  console.log(calculate(input(`${__dirname}/input.txt`).map(Number), 3))
 }
 init()
--- years/2020/01/solutions.ts
+++ years/2020/01/solutions.ts
@@ -1,3 +1,5 @@
+import { combinations } from '../../../utils/helpers'
+
 export const calculate = (input: number[], num: number) => {
   const match = combinations(input, num).find((combination: number[]) => {
     return combination.reduce((calculation: number, currentNumber: number) => calculation + currentNumber, 0) === 2020
@@ -8,26 +10,4 @@ export const calculate = (input: number[], num: number) => {
   }
 
   return match.reduce((a: number, b: number) => a * b, 1)
-}
-
-function combinations(set: number[], k: number): number[][] {
-  if (k > set.length) return []
-  if (k == set.length) return [set]
-
-  if (k == 1) {
-    let con: number[][] = []
-    set.forEach((_, index: number) => {
-      con.push([set[index]])
-    })
-    return con
-  }
-
-  let con: number[][] = []
-  for (let i = 0; i < set.length - k + 1; i++) {
-    let head = set.slice(i, i + 1)
-    let tail = combinations(set.slice(i + 1), k - 1)
-    tail.forEach((_: number[], index: number) => con.push(head.concat(tail[index])))
-  }
-
-  return con
 }
