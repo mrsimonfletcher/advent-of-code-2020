b6a431818de8cb2df37551c9887e4e2e2a2eb279
Simon Fletcher <simonfletcher0@gmail.com>
2020-12-24 12:38:31 +0000

    day 2 solution

---
+++ years/2020/02/index.ts
@@ -0,0 +1,13 @@
+import { part1, part2 } from "./solutions"
+import { input } from "../../../utils/helpers"
+
+(() => {
+  console.log(
+    part1(input(__dirname + `/input.txt`))
+  )
+
+  console.log(
+    part2(input(__dirname + `/input.txt`))
+  )
+})()
+
---
+++ years/2020/02/solutions.ts
@@ -0,0 +1,42 @@
+export const part1 = (input: string[]) => {
+  const matches = input.filter((policy: string) => {
+    const [atLeast, atMost, password, stringToMatch] = regex_match(policy)
+
+    const stringMatchCount = password.split('').reduce((accumulator: number, character: string) => {
+      if (character == stringToMatch) accumulator += 1
+      return accumulator
+    }, 0)
+
+    if(stringMatchCount >= parseInt(atLeast) && stringMatchCount <= parseInt(atMost)) {
+      return true
+    }
+
+    return false
+  })
+
+  return matches.length
+}
+
+export const part2 = (input: string[]) => {
+  const matches = input.filter((policy: string) => {
+    const [position1, position2, password, stringToMatch] = regex_match(policy)
+
+    if(
+      (password.split('')[parseInt(position1)-1] == stringToMatch && password.split('')[parseInt(position2)-1] != stringToMatch) ||
+      (password.split('')[parseInt(position1)-1] != stringToMatch && password.split('')[parseInt(position2)-1] == stringToMatch)
+     ) {
+       return true
+     }
+
+    return false
+  })
+
+  return matches.length
+}
+
+function regex_match(policy: string) {
+  const regex = policy.match(/(?<position1>\d*)-(?<position2>\d*)\s(?<string_to_match>[a-zA-Z]):\s(?<password>.*)/)
+  if (!regex || !regex.groups) throw new Error(`Sorry, couldn't find a match`)
+
+  return [regex.groups.position1, regex.groups.position2, regex.groups.password, regex.groups.string_to_match]
+}
