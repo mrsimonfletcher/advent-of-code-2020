9f9de35760447864c29bcc21129f11e16c755dec
Simon Fletcher <simonfletcher0@gmail.com>
2020-12-24 12:51:16 +0000

    Add prettier

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,10 +1,7 @@
-import * as fs from "fs"
+import * as fs from 'fs'
 
 export function input(file_path: string): string[] {
-  const array = fs
-    .readFileSync(file_path)
-    .toString()
-    .split(/\n/)
+  const array = fs.readFileSync(file_path).toString().split(/\n/)
 
   // Remove the last element from array which is always empty.
   array.pop()
