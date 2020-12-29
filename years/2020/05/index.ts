b319cd4580f7fa0edc2ea73b2cd2a07c18b795da
Simon Fletcher <simonfletcher0@gmail.com>
2020-12-29 11:12:20 +0000

    refactor

--- years/2020/05/index.ts
+++ years/2020/05/index.ts
@@ -1,7 +1,7 @@
-import * as fs from 'fs'
 import { input } from '../../../utils/helpers'
 import { part1, part2 } from './solutions'
 
+// Create input.txt file and add contents for this to run!
 const init = () => {
   console.log(part1(input(__dirname + `/input.txt`)))
   console.log(part2(input(__dirname + `/input.txt`)))
