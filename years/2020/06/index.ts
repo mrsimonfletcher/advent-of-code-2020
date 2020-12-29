da30b887503e9ce8bb4118d7af78802701cb4698
Simon Fletcher <simonfletcher0@gmail.com>
2020-12-29 11:16:43 +0000

    refactor

--- years/2020/06/index.ts
+++ years/2020/06/index.ts
@@ -1,7 +1,7 @@
-import * as fs from 'fs'
 import { part1, part2 } from './solutions'
 import { input } from '../../../utils/helpers'
 
+// Create input.txt file and add contents for this to run!
 const init = () => {
   const inputArray = input(`${__dirname}/input.txt`, false, /\n\n/)
   console.log(part1(inputArray))
