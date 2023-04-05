e4e219f80a9633c9c12e14399d5f146a3f08f95f
Simon Fletcher <simonfletcher0@gmail.com>
2023-04-05 08:52:37 +0100

    more idomatic JS

--- years/2019/01/solutions.ts
+++ years/2019/01/solutions.ts
@@ -1,16 +1,11 @@
-export const part1 = (input: string[]): number => {
-  return input.reduce((acc: number, value: string) => (acc += calcuation(parseInt(value))), 0)
-}
+export const part1 = (input: string[]): number => input.reduce((acc, value) => acc + calculation(Number(value)), 0)
+export const part2 = (input: string[]): number => input.reduce((acc, value) => acc + calculatePart2(Number(value)), 0)
 
-export const part2 = (input: string[]): number => {
-  return input.reduce((acc: number, value: string) => (acc += calculatePart2(parseInt(value))), 0)
-}
+const calculation = (value: number): number => Math.floor(value / 3) - 2
 
 const calculatePart2 = (value: number, acc: number = 0): number => {
-  const newVal = calcuation(value)
+  const newVal = calculation(value)
 
   if (newVal <= 0) return acc
-  return calculatePart2(newVal, (acc += newVal))
+  return calculatePart2(newVal, acc + newVal)
 }
-
-const calcuation = (value: number): number => Math.floor(value / 3) - 2
